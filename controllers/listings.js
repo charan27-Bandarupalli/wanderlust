338e326cd8f67a65afb4efc8b1890b1f25ad314d
charan27-Bandarupalli <coddingnibbas.2706@gmail.com>
2024-08-22 18:12:11 +0530

    some minor fixes 2

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,6 +4,8 @@ const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 
 // Check if the mapToken is set properly
 const mapToken = process.env.MAP_TOKEN;
+console.log("MAP_TOKEN",mapToken);
+
 if (!mapToken) {
     console.error('Error: Mapbox access token is missing. Please set the map_Token environment variable.');
     process.exit(1); // Exit the application if the token is not set
